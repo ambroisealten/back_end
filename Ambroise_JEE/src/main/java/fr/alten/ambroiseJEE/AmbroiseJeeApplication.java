f260e5464a5e11c56446a2735cac2a06031fab0b
mmaquinghen <maxime.maquinghen@alten.com>
2019-04-01 11:46:12 +0200

    Projet JEE
    Project initialization

---
+++ Ambroise_JEE/src/main/java/fr/alten/ambroiseJEE/AmbroiseJeeApplication.java
@@ -0,0 +1,13 @@
+package fr.alten.ambroiseJEE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmbroiseJeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmbroiseJeeApplication.class, args);
+	}
+
+}
