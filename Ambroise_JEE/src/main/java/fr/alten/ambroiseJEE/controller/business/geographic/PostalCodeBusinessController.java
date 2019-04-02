115b0bcb5f82c1ccc1c2c532bf6e177b35a7ce32
Andy CHABALIER <andy.chabalier@alten.com>
2019-04-02 17:54:34 +0200

    Implementing postal code Web Service

--- Ambroise_JEE/src/main/java/fr/alten/ambroiseJEE/controller/business/geographic/PostalCodeBusinessController.java
+++ Ambroise_JEE/src/main/java/fr/alten/ambroiseJEE/controller/business/geographic/PostalCodeBusinessController.java
@@ -3,27 +3,93 @@
  */
 package fr.alten.ambroiseJEE.controller.business.geographic;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.databind.JsonNode;
+
 import fr.alten.ambroiseJEE.model.beans.PostalCode;
+import fr.alten.ambroiseJEE.model.entityControllers.PostalCodeEntityController;
+import fr.alten.ambroiseJEE.security.Roles;
+import fr.alten.ambroiseJEE.utils.httpStatus.ConflictException;
+import fr.alten.ambroiseJEE.utils.httpStatus.CreatedException;
+import fr.alten.ambroiseJEE.utils.httpStatus.ForbiddenException;
+import fr.alten.ambroiseJEE.utils.httpStatus.HttpException;
+import fr.alten.ambroiseJEE.utils.httpStatus.RessourceNotFoundException;
 
 /**
+ * City controller for business rules.
  * @author Andy Chabalier
  *
  */
 @Service
 public class PostalCodeBusinessController {
 
+	@Autowired
+	private PostalCodeEntityController postalCodeEntityController;
+	
+
+	public Optional<PostalCode> getPostalCode(String name) {
+		return postalCodeEntityController.getPostalCode(name);
+	}
+
+
 	/**
-	 * @param placeName
-	 * @return
+	 * Method to delegate postalCode creation
+	 * @param jUser JsonNode with all city parameters
+	 * @return the @see {@link HttpException} corresponding to the statut of the
+	 *         request ({@link ConflictException} if there is a conflict in the
+	 *         database and {@link CreatedException} if the postalCode is created
 	 * @author Andy Chabalier
 	 */
-	public Optional<PostalCode> getPostalCode(String placeName) {
-		// TODO Auto-generated method stub
-		return null;
+	public HttpException createPostalCode(JsonNode jPostalCode, int role) {
+		return Roles.ADMINISTRATOR_USER_ROLE.getValue()== role ? postalCodeEntityController.createPostalCode(jPostalCode) : new ForbiddenException();
+	}
+
+
+	/**
+	 * @param role the user role
+	 * @return the list of all ppostalCodes
+	 * @author Andy Chabalier
+	 */
+	public List<PostalCode> getPostalCodes(int role) {
+		if (Roles.ADMINISTRATOR_USER_ROLE.getValue()== role) {
+			return postalCodeEntityController.getPostalCodes();
+		}
+		throw new ForbiddenException();	
+	}
+
+
+	/**
+	 * 
+	 * @param jPostalCode JsonNode with all postalCode parameters and the old name to perform the update even if the name is changed
+	 * @param role user role
+	 * @return the @see {@link HttpException} corresponding to the statut of the
+	 *         request ({@link RessourceNotFoundException} if the ressource is not found
+	 *         and {@link CreatedException} if the postalCode is updated
+	 * @author Andy Chabalier
+	 */
+	public HttpException updatePostalCode(JsonNode jPostalCode, int role) {
+		return Roles.ADMINISTRATOR_USER_ROLE.getValue()== role ? postalCodeEntityController.updatePostalCode(jPostalCode) : new ForbiddenException();
+	}
+
+
+	/**
+	 * 
+	 * @param params the postalCode name to delete 
+	 * @param role the user role
+	 * @return @see {@link HttpException} corresponding to the statut of the
+	 *         request ({@link ForbiddenException} if the ressource is not found
+	 *         and {@link CreatedException} if the postalCode is deleted
+	 * @author Andy Chabalier
+	 */
+	public HttpException deletePostalCode(JsonNode params, int role) {
+		return Roles.ADMINISTRATOR_USER_ROLE.getValue()== role ? postalCodeEntityController.deletePostalCode(params.get("name").textValue()) : new ForbiddenException();
 	}
 
 }
+
+
---
+++ Ambroise_JEE/src/main/java/fr/alten/ambroiseJEE/controller/rest/PostalCodeRestController.java
@@ -0,0 +1,74 @@
+/**
+ * 
+ */
+package fr.alten.ambroiseJEE.controller.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import fr.alten.ambroiseJEE.controller.business.geographic.PostalCodeBusinessController;
+import fr.alten.ambroiseJEE.utils.httpStatus.HttpException;
+import fr.alten.ambroiseJEE.utils.httpStatus.UnprocessableEntityException;
+
+/**
+ * Rest controller for the postal code web service
+ * 
+ * @author Andy Chabalier
+ *
+ */
+@Controller
+public class PostalCodeRestController {
+
+	@Autowired
+	private PostalCodeBusinessController postalCodeBusinessController;
+
+	private final Gson gson;
+
+	public PostalCodeRestController() {
+		GsonBuilder builder = new GsonBuilder();
+		this.gson = builder.create();
+	}
+
+	@PostMapping(value = "/postalCode")
+	@ResponseBody
+	public HttpException createPostalCode(@RequestBody JsonNode params, @RequestAttribute("mail") String mail,
+			@RequestAttribute("role") int role) throws Exception {
+		return params.get("mail") != null ? postalCodeBusinessController.createPostalCode(params, role)
+				: new UnprocessableEntityException();
+	}
+
+	@GetMapping(value = "/postalCodes")
+	@ResponseBody
+	public String getPostalCodes(@RequestAttribute("mail") String mail, @RequestAttribute("role") int role) {
+		return gson.toJson(postalCodeBusinessController.getPostalCodes(role));
+	}
+
+	@PutMapping(value = "/postalCode")
+	@ResponseBody
+	public HttpException updatePostalCode(@RequestBody JsonNode params, @RequestAttribute("mail") String mail,
+			@RequestAttribute("role") int role) throws Exception {
+		return params.get("mail") != null ? postalCodeBusinessController.createPostalCode(params, role)
+				: new UnprocessableEntityException();
+	}
+
+	@DeleteMapping(value = "/postalCode")
+	@ResponseBody
+	public HttpException deletePostalCode(@RequestBody JsonNode params, @RequestAttribute("mail") String mail,
+			@RequestAttribute("role") int role) throws Exception {
+		return params.get("mail") != null ? postalCodeBusinessController.deletePostalCode(params, role)
+				: new UnprocessableEntityException();
+	}
+}
+
+
