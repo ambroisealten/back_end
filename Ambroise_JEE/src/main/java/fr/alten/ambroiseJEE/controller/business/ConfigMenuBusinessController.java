0236090596869a75dcb79d6a60d35c020b405358
Camille SCHNELL <camille.schnell@alten.com>
2019-04-04 11:04:52 +0200

    configMenu webservice with new UserRole enum

--- Ambroise_JEE/src/main/java/fr/alten/ambroiseJEE/controller/business/ConfigMenuBusinessController.java
+++ Ambroise_JEE/src/main/java/fr/alten/ambroiseJEE/controller/business/ConfigMenuBusinessController.java
@@ -2,7 +2,7 @@ package fr.alten.ambroiseJEE.controller.business;
 
 import java.io.FileNotFoundException;
 
-import fr.alten.ambroiseJEE.security.Roles;
+import fr.alten.ambroiseJEE.security.UserRole;
 import fr.alten.ambroiseJEE.utils.config.ParseConfigFile;
 import fr.alten.ambroiseJEE.utils.httpStatus.ForbiddenException;
 
@@ -21,10 +21,9 @@ public class ConfigMenuBusinessController {
 	 * @throws FileNotFoundException
 	 *  {@link ForbiddenException} if the user isn't connected
 	 */
-	public String getItems(int role) throws FileNotFoundException {
-		if(Roles.ADMINISTRATOR_USER_ROLE.getValue() == role || Roles.DEFAULT_USER_ROLE.getValue() == role ) {
-			return "";//ParseConfigFile.getJsonMenuItems(role);
-		}
+	public String getItems(UserRole role) throws FileNotFoundException {
+		if(!(UserRole.DESACTIVATED == role))
+			return ParseConfigFile.getJsonMenuItemsByRole(role);
 		throw new ForbiddenException();
 	}
 }
