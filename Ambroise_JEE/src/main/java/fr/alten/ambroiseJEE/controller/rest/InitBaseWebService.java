55daa0bccf27eb7ede35028700d84838f11cacd9
Andy CHABALIER <andy.chabalier@alten.com>
2019-04-05 13:28:57 +0200

    refactoring user creation in a separated method

--- Ambroise_JEE/src/main/java/fr/alten/ambroiseJEE/controller/rest/InitBaseWebService.java
+++ Ambroise_JEE/src/main/java/fr/alten/ambroiseJEE/controller/rest/InitBaseWebService.java
@@ -45,90 +45,84 @@ public class InitBaseWebService {
 	@PostMapping(value = "/admin/init")
 	@ResponseBody
 	public HttpException init() throws IOException {
-		
-		//Creation d'une population de base
-		
-		User userAdmin = new User();
-		userAdmin.setForname("tempUserAdmin");
-		userAdmin.setMail("tempUserAdmin@mail.com");
-		userAdmin.setName("tempUserAdminName");
-		userAdmin.setPswd("pass");
-		userAdmin.setRole(UserRole.MANAGER_ADMIN);
-		
-		User userManager = new User();
-		userManager.setForname("tempUserManager");
-		userManager.setMail("tempUserManager@mail.com");
-		userManager.setName("tempUserManagerName");
-		userManager.setPswd("pass");
-		userManager.setRole(UserRole.MANAGER);
-		
-		User userCDR = new User();
-		userCDR.setForname("tempUserCDR");
-		userCDR.setMail("tempUserCDR@mail.com");
-		userCDR.setName("tempUserCDRName");
-		userCDR.setPswd("pass");
-		userCDR.setRole(UserRole.CDR);
-		
-		User userConsultant = new User();
-		userConsultant.setForname("tempUserConsultant");
-		userConsultant.setMail("tempUserConsultant@mail.com");
-		userConsultant.setName("tempUserConsultantName");
-		userConsultant.setPswd("pass");
-		userConsultant.setRole(UserRole.CONSULTANT);
-		
-		User userDesactivated = new User();
-		userDesactivated.setForname("");
-		userDesactivated.setMail("desactivated" + System.currentTimeMillis());
-		userDesactivated.setName("");
-		userDesactivated.setPswd("");
-		userDesactivated.setRole(UserRole.DESACTIVATED);
-		
-		JsonNode userAdminJsonNode = toJsonNode(gson.toJsonTree(userAdmin).getAsJsonObject());
-		((ObjectNode) userAdminJsonNode).put("pswd", "pass");
-		((ObjectNode) userAdminJsonNode).put("role", "MANAGER_ADMIN");
-		((ObjectNode) userAdminJsonNode).put("agency", "");
-		JsonNode userManagerJsonNode = toJsonNode(gson.toJsonTree(userManager).getAsJsonObject());
-		((ObjectNode) userManagerJsonNode).put("pswd", "pass");
-		((ObjectNode) userManagerJsonNode).put("role", "MANAGER");
-		((ObjectNode) userManagerJsonNode).put("agency", "");
-		JsonNode userCDRJsonNode = toJsonNode(gson.toJsonTree(userCDR).getAsJsonObject());
-		((ObjectNode) userCDRJsonNode).put("pswd", "pass");
-		((ObjectNode) userCDRJsonNode).put("role", "CDR");
-		((ObjectNode) userCDRJsonNode).put("agency", "");
-		JsonNode userConsultantJsonNode = toJsonNode(gson.toJsonTree(userConsultant).getAsJsonObject());
-		((ObjectNode) userConsultantJsonNode).put("pswd", "pass");
-		((ObjectNode) userConsultantJsonNode).put("role", "CONSULTANT");
-		((ObjectNode) userConsultantJsonNode).put("agency", "");
-		JsonNode userDesactivatedJsonNode = toJsonNode(gson.toJsonTree(userDesactivated).getAsJsonObject());
-		((ObjectNode) userDesactivatedJsonNode).put("pswd", "pass");
-		((ObjectNode) userDesactivatedJsonNode).put("role", "DESACTIVATED");
-		((ObjectNode) userDesactivatedJsonNode).put("agency", "");
 
-		userBusinessController.createUser(userAdminJsonNode,UserRole.MANAGER_ADMIN);
-		userBusinessController.createUser(userManagerJsonNode,UserRole.MANAGER_ADMIN);
-		userBusinessController.createUser(userCDRJsonNode,UserRole.MANAGER_ADMIN);
-		userBusinessController.createUser(userConsultantJsonNode,UserRole.MANAGER_ADMIN);
-		userBusinessController.createUser(userDesactivatedJsonNode,UserRole.MANAGER_ADMIN);
+		createUsers();
 		
-		//Remplissage d'une population de consultant
-		for(int i = 0; i<10 ; i++) {
-			User useri = new User();
-			useri.setForname("tempUserConsultant"+i);
-			useri.setMail("tempUserconsultant"+i+"@mail.com");
-			useri.setName("tempUserAdminName");
-			useri.setPswd("pass");
-			useri.setRole(UserRole.CONSULTANT);
-			
-			JsonNode useriJsonNode = toJsonNode(gson.toJsonTree(useri).getAsJsonObject());
-			((ObjectNode) useriJsonNode).put("pswd", "pass");
-			((ObjectNode) useriJsonNode).put("role", "DESACTIVATED");
-			((ObjectNode) useriJsonNode).put("agency", "");
-
-			userBusinessController.createUser(useriJsonNode,UserRole.MANAGER_ADMIN);		}
+		
 		return new CreatedException();
 	}
+
 	
-	
+	private void createUsers() throws IOException {
+		// Creation d'une population de base
+
+				User userAdmin = new User();
+				userAdmin.setForname("tempUserAdmin");
+				userAdmin.setMail("tempUserAdmin@mail.com");
+				userAdmin.setName("tempUserAdminName");
+
+				User userManager = new User();
+				userManager.setForname("tempUserManager");
+				userManager.setMail("tempUserManager@mail.com");
+				userManager.setName("tempUserManagerName");
+
+				User userCDR = new User();
+				userCDR.setForname("tempUserCDR");
+				userCDR.setMail("tempUserCDR@mail.com");
+				userCDR.setName("tempUserCDRName");
+
+				User userConsultant = new User();
+				userConsultant.setForname("tempUserConsultant");
+				userConsultant.setMail("tempUserConsultant@mail.com");
+				userConsultant.setName("tempUserConsultantName");
+
+				User userDesactivated = new User();
+				userDesactivated.setForname("");
+				userDesactivated.setMail("desactivated" + System.currentTimeMillis());
+				userDesactivated.setName("");
+
+				JsonNode userAdminJsonNode = toJsonNode(gson.toJsonTree(userAdmin).getAsJsonObject());
+				((ObjectNode) userAdminJsonNode).put("pswd", "pass");
+				((ObjectNode) userAdminJsonNode).put("role", "MANAGER_ADMIN");
+				((ObjectNode) userAdminJsonNode).put("agency", "");
+				JsonNode userManagerJsonNode = toJsonNode(gson.toJsonTree(userManager).getAsJsonObject());
+				((ObjectNode) userManagerJsonNode).put("pswd", "pass");
+				((ObjectNode) userManagerJsonNode).put("role", "MANAGER");
+				((ObjectNode) userManagerJsonNode).put("agency", "");
+				JsonNode userCDRJsonNode = toJsonNode(gson.toJsonTree(userCDR).getAsJsonObject());
+				((ObjectNode) userCDRJsonNode).put("pswd", "pass");
+				((ObjectNode) userCDRJsonNode).put("role", "CDR");
+				((ObjectNode) userCDRJsonNode).put("agency", "");
+				JsonNode userConsultantJsonNode = toJsonNode(gson.toJsonTree(userConsultant).getAsJsonObject());
+				((ObjectNode) userConsultantJsonNode).put("pswd", "pass");
+				((ObjectNode) userConsultantJsonNode).put("role", "CONSULTANT");
+				((ObjectNode) userConsultantJsonNode).put("agency", "");
+				JsonNode userDesactivatedJsonNode = toJsonNode(gson.toJsonTree(userDesactivated).getAsJsonObject());
+				((ObjectNode) userDesactivatedJsonNode).put("pswd", "pass");
+				((ObjectNode) userDesactivatedJsonNode).put("role", "DESACTIVATED");
+				((ObjectNode) userDesactivatedJsonNode).put("agency", "");
+
+				userBusinessController.createUser(userAdminJsonNode, UserRole.MANAGER_ADMIN);
+				userBusinessController.createUser(userManagerJsonNode, UserRole.MANAGER_ADMIN);
+				userBusinessController.createUser(userCDRJsonNode, UserRole.MANAGER_ADMIN);
+				userBusinessController.createUser(userConsultantJsonNode, UserRole.MANAGER_ADMIN);
+				userBusinessController.createUser(userDesactivatedJsonNode, UserRole.MANAGER_ADMIN);
+
+				// Remplissage d'une population de consultant
+				for (int i = 0; i < 10; i++) {
+					User useri = new User();
+					useri.setForname("tempUserConsultant" + i);
+					useri.setMail("tempUserconsultant" + i + "@mail.com");
+					useri.setName("tempUserAdminName");
+
+					JsonNode useriJsonNode = toJsonNode(gson.toJsonTree(useri).getAsJsonObject());
+					((ObjectNode) useriJsonNode).put("pswd", "pass");
+					((ObjectNode) useriJsonNode).put("role", "CONSULTANT");
+					((ObjectNode) useriJsonNode).put("agency", "");
+
+					userBusinessController.createUser(useriJsonNode, UserRole.MANAGER_ADMIN);
+				}
+	}
 	private JsonNode toJsonNode(JsonObject jsonObj) throws IOException {
 		ObjectMapper objectMapper = new ObjectMapper();
 		return objectMapper.readTree(jsonObj.toString());
