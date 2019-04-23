fd90142060032454e9d4112b8e88041b69502f90
Andy CHABALIER <andy.chabalier@alten.com>
2019-04-23 17:21:25 +0200

    new implementation on how work upload file and file bean

--- Ambroise_JEE/src/main/java/fr/alten/ambroiseJEE/controller/rest/FileRestController.java
+++ Ambroise_JEE/src/main/java/fr/alten/ambroiseJEE/controller/rest/FileRestController.java
@@ -9,6 +9,7 @@ import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
