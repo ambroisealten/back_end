f29adaf0c43fc32775e7c72bd2e525e1ce1ff91a
AltenKylian <kylian.gehier@alten.com>
2019-05-17 09:38:54 +0200

    removing abstract of Availability causing InstanciationError when getting a skillSheet having availability field

--- Ambroise_JEE/src/main/java/fr/alten/ambroiseJEE/utils/availability/Availability.java
+++ Ambroise_JEE/src/main/java/fr/alten/ambroiseJEE/utils/availability/Availability.java
@@ -1,14 +1,12 @@
 package fr.alten.ambroiseJEE.utils.availability;
 
-import java.io.Serializable;
-
 /**
  *
  * @author Kylian Gehier
  *
  */
 
-public abstract class Availability{
+public /*abstract*/ class Availability{
 
 	private long initDate;
 
