1d502207c183e448bdfacee4e59309c9f0e858df
ThomasDecampAlten <thomas.decamp@alten.com>
2019-05-24 10:55:58 +0200

    Message Erreur V2

--- Ambroise_JEE/src/main/java/fr/alten/ambroiseJEE/controller/rest/LoginRestController.java
+++ Ambroise_JEE/src/main/java/fr/alten/ambroiseJEE/controller/rest/LoginRestController.java
@@ -17,7 +17,6 @@ import fr.alten.ambroiseJEE.controller.business.UserBusinessController;
 import fr.alten.ambroiseJEE.security.JWTokenUtility;
 import fr.alten.ambroiseJEE.security.Token;
 import fr.alten.ambroiseJEE.utils.httpStatus.ForbiddenException;
-import fr.alten.ambroiseJEE.utils.httpStatus.ForbiddenExceptionLogin;
 import fr.alten.ambroiseJEE.utils.httpStatus.UnprocessableEntityException;
 
 /**
@@ -55,7 +54,7 @@ public class LoginRestController {
 			final String pswd = params.get("pswd").textValue();
 
 			final String subject = this.userBusinessController.checkIfCredentialIsValid(mail, pswd)
-					.orElseThrow(ForbiddenExceptionLogin::new);
+					.orElseThrow(ForbiddenException::new);
 			// Si un sujet est present, alors l'utilisateur existe bien. On construit son
 			// token
 			final Token jsonResponse = JWTokenUtility.buildJWT(subject);
