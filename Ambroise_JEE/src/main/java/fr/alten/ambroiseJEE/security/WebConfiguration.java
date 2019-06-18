4ce3a07f6ca31863ba205ae717e400bef6e33d07
Andy CHABALIER <andy.chabalier@alten.com>
2019-06-18 14:57:06 +0200

    HOTFIX: for deployment on ubuntu server, and addition of CORS allow origin parameters in application.properties

--- Ambroise_JEE/src/main/java/fr/alten/ambroiseJEE/security/WebConfiguration.java
+++ Ambroise_JEE/src/main/java/fr/alten/ambroiseJEE/security/WebConfiguration.java
@@ -1,5 +1,7 @@
 package fr.alten.ambroiseJEE.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,9 +9,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
 
+	@Autowired
+	private ApplicationContext ctx;
+
 	@Override
 	public void addCorsMappings(final CorsRegistry registry) {
-		registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("GET", "PUT", "POST",
-				"DELETE", "OPTIONS");
+		registry.addMapping("/**")
+				.allowedOrigins("http://localhost:4200", ctx.getEnvironment().getProperty("security.allowOrigin"))
+				.allowedMethods("GET", "PUT", "POST", "DELETE", "OPTIONS");
 	}
 }
